0e192cadf7cf5b5aba9b23283bdc732f67365ad5
mstombaugh <mstombaugh@tamu.edu>
2014-02-26 18:38:21 -0600

    Remade login screen

---
+++ PromenadeVT/db_config.php
@@ -0,0 +1,11 @@
+<?php
+ 
+/*
+ * All database connection variables
+ */
+ 
+define('DB_USER', "awsuser"); // db user
+define('DB_PASSWORD', "promenade"); // db password (mention your db password here)
+define('DB_DATABASE', "promenadevt"); // database name
+define('DB_SERVER', "promenadevt.cm8ddcjx8fni.us-west-2.rds.amazonaws.com"); // db server
+?>
