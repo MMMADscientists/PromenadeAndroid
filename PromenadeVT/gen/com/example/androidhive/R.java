e5746ddcfdfde064200528303cbca978db76e318
ANixon124 <cobra124703@yahoo.com>
2014-03-23 20:11:33 -0500

    added property and room pages
    added backbone of these pages, makes the calls to the database but does
    not interpret it yet, and code for action upon button clicks needs to be
    added.

--- PromenadeVT/gen/com/example/androidhive/R.java
+++ PromenadeVT/gen/com/example/androidhive/R.java
@@ -24,24 +24,28 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000c;
-        public static final int btnLinkToLoginScreen=0x7f08000b;
-        public static final int btnLinkToRegisterScreen=0x7f080005;
-        public static final int btnLogin=0x7f080004;
+        public static final int action_settings=0x7f08000e;
+        public static final int btnLinkToLoginScreen=0x7f08000d;
+        public static final int btnLinkToRegisterScreen=0x7f080007;
+        public static final int btnLogin=0x7f080006;
         public static final int btnLogout=0x7f080000;
-        public static final int btnRegister=0x7f08000a;
-        public static final int loginPassword=0x7f080002;
-        public static final int loginUser=0x7f080001;
-        public static final int login_error=0x7f080003;
-        public static final int registerEmail=0x7f080007;
-        public static final int registerName=0x7f080006;
-        public static final int registerPassword=0x7f080008;
-        public static final int register_error=0x7f080009;
+        public static final int btnRegister=0x7f08000c;
+        public static final int homesLayout=0x7f080001;
+        public static final int loginPassword=0x7f080004;
+        public static final int loginUser=0x7f080003;
+        public static final int login_error=0x7f080005;
+        public static final int registerEmail=0x7f080009;
+        public static final int registerName=0x7f080008;
+        public static final int registerPassword=0x7f08000a;
+        public static final int register_error=0x7f08000b;
+        public static final int textView1=0x7f080002;
     }
     public static final class layout {
         public static final int dashboard=0x7f030000;
-        public static final int login=0x7f030001;
-        public static final int register=0x7f030002;
+        public static final int homes=0x7f030001;
+        public static final int login=0x7f030002;
+        public static final int register=0x7f030003;
+        public static final int rooms=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
