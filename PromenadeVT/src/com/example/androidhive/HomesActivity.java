d3f66dae6d7a3cddb17014c18c2f605be8e40ce2
mstombaugh <mstombaugh001@gmail.com>
2014-03-24 15:48:21 -0500

    grabbed JSON data and put into Lists

--- PromenadeVT/src/com/example/androidhive/HomesActivity.java
+++ PromenadeVT/src/com/example/androidhive/HomesActivity.java
@@ -24,7 +24,16 @@ public class HomesActivity extends Activity
 	List<Button> properties;
 	Button btnNewProp;
 	private static String username;
-	private static String dbID;
+	
+	// JSON Response node names
+    private static String KEY_SUCCESS = "success";
+    private static String KEY_ERROR = "error";
+    private static String KEY_ERROR_MSG = "error_msg";
+    private static String KEY_TUPLE = "tuples";
+    private static String KEY_IDPROPERTY = "idProperty";
+    private static String KEY_ADDRESS = "address";
+    private static String KEY_HOUSEURL = "houseUrl";
+    private static String KEY_NAME = "name";
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) 
@@ -36,17 +45,36 @@ public class HomesActivity extends Activity
 		// may need to account for newly registered user here
 		Intent intent = getIntent();
 		username = intent.getStringExtra("name");// pull from previous page
-		dbID = intent.getStringExtra("id");
+		//dbID = intent.getStringExtra("id");
 		
 		// make database call
 		UserFunctions userFunction = new UserFunctions();
-        JSONObject json = userFunction.getHomes(dbID);
-         
-        // parse json to pull property info
-        for(int i = 0; i < json.length(); i++)
-        {
+        	JSONObject json = userFunction.getHomes(username);
         	
-        }
+        	//lists of recieved data
+        	List<int> idProperty = new List<int>();
+        	List<String> address = new List<String>();
+        	List<String> houseURL = new List<String>();
+        	
+        	 try {
+                    if (json.getString(KEY_SUCCESS) != null) {
+                        //loginErrorMsg.setText("");
+                        String res = json.getString(KEY_SUCCESS); 
+                        if(Integer.parseInt(res) == 1){
+                        	JSONArray tuples = json.getJSONArray(KEY_TUPLE);
+                        	for int i = 0; i< tuples.length(); i++){
+                        		JSONObject curTuple = tuples.get(i);
+                        		idProperty.add(curTuple.getInt(KEY_IDPROPERTY);
+                        		address.add(curTuple.getString(KEY_ADDRESS);
+                        		houseURL.add(curTuple.getString(KEY_HOUSEURL);
+                        	}
+                        }
+                            
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+         
         
 		// fill xml based on db call
         LinearLayout ll = (LinearLayout) findViewById(R.id.homesLayout);
@@ -55,7 +83,7 @@ public class HomesActivity extends Activity
                 LinearLayout.LayoutParams.WRAP_CONTENT);
         
         // may need separate file
-        for(int i = 0; i < json.length(); i++)
+        for(int i = 0; i < idProperty.length(); i++)
         {
         	
             Button btn = new Button(this);
@@ -67,13 +95,13 @@ public class HomesActivity extends Activity
             // fill properties based on db call
             properties.add((Button) findViewById(id_));
         }
-        
+        */
         
         Button btn = new Button(this);
-        btn.setId(json.length());
+        btn.setId(idProperty.length());
         btn.setText("Add new property +");
         ll.addView(btn, params);
-        btnNewProp = (Button) findViewById(json.length());
+        btnNewProp = (Button) findViewById(idProperty.length());
        
 		
 		
