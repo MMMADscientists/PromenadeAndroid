c17a74b56dc7c5d661b4ebd3ba3b5707a1d1aade
mstombaugh <mstombaugh001@gmail.com>
2014-03-31 16:32:07 -0500

    Update DatabaseAccessTask.java

--- PromenadeVT/src/com/example/androidhive/library/DatabaseAccessTask.java
+++ PromenadeVT/src/com/example/androidhive/library/DatabaseAccessTask.java
@@ -20,6 +20,14 @@ import org.json.JSONObject;
 import android.util.Log;
 
 public class DatabaseAccessTask extends android.os.AsyncTask<String, Void, JSONObject> {
+	
+	 private static String login_tag = "login";
+	 private static String register_tag = "register";
+	    private static String houses_tag = "houses";
+	    private static String rooms_tag = "rooms";
+	    private static String connections_tag = "connections";
+	    private static String property_rename_tag = "renameProperty";
+	    private static String room_rename_tag = "renameRoom";
 
 	private static String loginURL = "http://54.186.153.0/API/index.php";
     private static String registerURL = "http://54.186.153.0/API/index.php";
@@ -29,14 +37,40 @@ public class DatabaseAccessTask extends android.os.AsyncTask<String, Void, JSONO
 	protected JSONObject doInBackground(String... params) {
 		// TODO Auto-generated method stub
 		int count = params.length;
-		//login
-		if(count == 3){
+		//houses
+		if(params[0] == houses_tag){
 			List<NameValuePair> param = new ArrayList<NameValuePair>();
-	        param.add(new BasicNameValuePair("tag", params[0]));
-	        param.add(new BasicNameValuePair("username", params[1]));
-	        param.add(new BasicNameValuePair("password", params[2]));
-	        JSONObject json = jsonParser.getJSONFromUrl(loginURL, param);
-	        return json;
+			param.add(new BasicNameValuePair("tag", params[0]));
+			param.add(new BasicNameValuePair("houses", params[0]));
+		}//rooms
+		else if(params[0] == rooms_tag){
+			List<NameValuePair> param = new ArrayList<NameValuePair>();
+			param.add(new BasicNameValuePair("tag", params[0]));
+			param.add(new BasicNameValuePair("propertyID", params[0]));
+		}//rename property
+		else if(params[0] == property_rename_tag){
+			List<NameValuePair> param = new ArrayList<NameValuePair>();
+        		param.add(new BasicNameValuePair("tag", params[0]));
+        		param.add(new BasicNameValuePair("propertyID", params[1]));
+        		param.add(new BasicNameValuePair("address", params[2]));
+        		JSONObject json = jsonParser.getJSONFromUrl(loginURL, param);
+	        	return json;
+		}//rename room
+		else if(params[0] == room_rename_tag){
+			List<NameValuePair> param = new ArrayList<NameValuePair>();
+        		param.add(new BasicNameValuePair("tag", params[0]));
+        		param.add(new BasicNameValuePair("roomID", params[1]));
+        		param.add(new BasicNameValuePair("roomName", params[2]));
+        		JSONObject json = jsonParser.getJSONFromUrl(loginURL, param);
+	        	return json;
+		}//login
+		else if(count == 3){
+			List<NameValuePair> param = new ArrayList<NameValuePair>();
+	        	param.add(new BasicNameValuePair("tag", params[0]));
+	        	param.add(new BasicNameValuePair("username", params[1]));
+	        	param.add(new BasicNameValuePair("password", params[2]));
+	        	JSONObject json = jsonParser.getJSONFromUrl(loginURL, param);
+	        	return json;
 		}//register
 		else if(count == 4){
 			List<NameValuePair> param = new ArrayList<NameValuePair>();
